b622caf963083110a5e497db7a0294955fa1ab56
Dylan Avila <166364578+DylanAvila04@users.noreply.github.com>
2025-10-08 14:34:50 -0700

    Added a button to main to show on website

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
-document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-`;
+// Create a new button
+const dialogButton = document.createElement('button');
+dialogButton.textContent = 'Click Me!';
+dialogButton.onclick = () => {
+  alert('Hello my name is Dylan and this is a start');
+};
+
+// Insert it into the page, just above the "Read the docs" paragraph
+const docs = document.querySelector('.read-the-docs');
+if (docs) {
+  docs.parentNode?.insertBefore(dialogButton, docs);
+}
+
