77d3578db465003456164e0bf40fdaed9f1ccbc3
Dylan Avila <166364578+DylanAvila04@users.noreply.github.com>
2025-10-08 21:44:44 +0000

    finished adding a button to new repository

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,7 @@
 import "./style.css";
 
 // Create a new button
-const dialogButton = document.createElement('button');
-dialogButton.textContent = 'Click Me!';
-dialogButton.onclick = () => {
-  alert('Hello my name is Dylan and this is a start');
-};
-
-// Insert it into the page, just above the "Read the docs" paragraph
-const docs = document.querySelector('.read-the-docs');
-if (docs) {
-  docs.parentNode?.insertBefore(dialogButton, docs);
-}
+document.body.innerHTML = `
+  <button>boo</button>
+`;
 
