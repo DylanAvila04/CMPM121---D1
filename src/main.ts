c793b6f5f09f5dd46b7c66f83f17b7be7d45f323
Dylan Avila <166364578+DylanAvila04@users.noreply.github.com>
2025-10-10 02:19:27 +0000

    Co-authored-by: QuestionMark-128 <QuestionMark-128@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,7 @@ document.body.innerHTML = `
   <button>boo</button>
 `;
 console.log("Yippee!!!!!");
+console.log("Here we ggo again");
 
 console.log("Yippeeee Again!!!!");
+console.log("YIPEEEEE ONCE MOREEEEE!!!!");
