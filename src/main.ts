31434926ce23231e8905ef759350ed904e4be095
Dylan Avila <166364578+DylanAvila04@users.noreply.github.com>
2025-10-11 22:49:47 +0000

    Made the first clickable Crossiant Button for my Bakery incremnetal game!

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import "./style.css";
 
+// style = "display : block, margin : 0 auto"; font - size: 20px;"
+
 // Create a new button
 document.body.innerHTML = `
-  <button>boo</button>
+  <button> 🥐 Click for a Crossiant</button>
 `;
+const button = document.querySelector("button");
+
+// Add an event listener to the button
+button?.addEventListener("click", () => {
+  alert("You have gained a crossiant!");
+}); 
