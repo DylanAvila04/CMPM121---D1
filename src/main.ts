1e2040e8f3cba41a99fe16e2e3a3841fceddd278
Dylan Avila <166364578+DylanAvila04@users.noreply.github.com>
2025-10-12 01:30:55 +0000

    Step 4 complete!

--- src/main.ts
+++ src/main.ts
@@ -11,16 +11,17 @@ document.body.innerHTML = `
 
 const button = document.querySelector("button");
 const counterElement = document.getElementById("Counter");
+const rateElement = document.getElementById("rate");
 // const cookElement = document.getElementById("cook");
 
 let counter = 0; // 0 crossiants to start
 const cookscrossiants = 1; // 0 crossiants from cook
 
-setInterval(() => {
-  counter += cookscrossiants; // increase counter by number of cooks
-  updateDisplay();
-  // updateCookDisplay();
-}, 1000); // every second
+// setInterval(() => {
+//   counter += cookscrossiants; // increase counter by number of cooks
+//   updateDisplay();
+//   // updateCookDisplay();
+// }, 1000); // every second
 
 // Add an event listener to the button
 button?.addEventListener("click", () => {
@@ -28,12 +29,34 @@ button?.addEventListener("click", () => {
   updateDisplay();
 });
 
+let pasttime: number | null = null;
+
+function loop(timestamp: number) {
+  if (!pasttime) pasttime = timestamp;
+  const diff = (timestamp - pasttime) / 1000;
+
+  counter += diff * cookscrossiants;
+  pasttime = timestamp;
+
+  updateDisplay();
+  requestAnimationFrame(loop);
+}
+
+requestAnimationFrame(loop);
+
 function updateDisplay() {
   if (counterElement) {
     counterElement.textContent = `You have ${counter} crossiants!🥐`; // update the counter display
   }
+  if (counterElement) {
+    const visible = Math.floor(counter);
+    counterElement.textContent = `You have ${visible} crossiants!🥐`; // update the counter display
+  }
+  if (rateElement) {
+    rateElement.textContent =
+      `You are making ${cookscrossiants} crossiants per second!🥐`; // update the rate display
+  }
 }
-
 // function updateCookDisplay() {
 //   if (cookElement) {
 //     cookElement.textContent =
