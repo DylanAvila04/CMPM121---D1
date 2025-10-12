2488ca49dcbfd8c3b79063adce6eb7d2338e9f1a
Dylan Avila <166364578+DylanAvila04@users.noreply.github.com>
2025-10-12 07:52:24 +0000

    Step 5 complete!

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,20 @@ import "./style.css";
 document.body.innerHTML = `
   <button> 🥐 </button>
   <div id = "Counter"> You have 0 crossiants!🥐</div>
-  <div id = "cook"> Click to hire a baker!👩‍🍳 </div>
+  <div id = "rate"> Passive income: 0.0 corssiants/sec 👩‍🍳</div>
+  <button id = "baker"> 10 crossiants = 1 Baker👩‍🍳 (Owned: 0) </button>
 `;
 
 const button = document.querySelector("button");
 const counterElement = document.getElementById("Counter");
 const rateElement = document.getElementById("rate");
+const bakerButton = document.getElementById("baker");
 // const cookElement = document.getElementById("cook");
 
 let counter = 0; // 0 crossiants to start
-const cookscrossiants = 1; // 0 crossiants from cook
+let cookscrossiants = 1; // 0 crossiants from cook
+let bakers = 0; // 0 cooks to start
+const bakerCost = 10; // cost of 1 cook
 
 // setInterval(() => {
 //   counter += cookscrossiants; // increase counter by number of cooks
@@ -29,37 +33,44 @@ button?.addEventListener("click", () => {
   updateDisplay();
 });
 
-let pasttime: number | null = null;
+bakerButton?.addEventListener("click", () => {
+  if (counter >= bakerCost) {
+    bakers++; // increase number of cooks by 1
+    counter -= bakerCost; // decrease counter by cost of cook
+    cookscrossiants = bakers; // each cook makes 1 crossiant per second
+    updateDisplay();
+    // updateCookDisplay();
+  }
+});
 
-function loop(timestamp: number) {
-  if (!pasttime) pasttime = timestamp;
-  const diff = (timestamp - pasttime) / 1000;
+  let pasttime: number | null = null;
 
-  counter += diff * cookscrossiants;
-  pasttime = timestamp;
+  function loop(timestamp: number) {
+    if (!pasttime) pasttime = timestamp;
+    const diff = (timestamp - pasttime) / 1000;
 
-  updateDisplay();
+    counter += diff * cookscrossiants;
+    pasttime = timestamp;
+
+    updateDisplay();
+    requestAnimationFrame(loop);
+  }
+
   requestAnimationFrame(loop);
-}
 
-requestAnimationFrame(loop);
-
-function updateDisplay() {
-  if (counterElement) {
-    counterElement.textContent = `You have ${counter} crossiants!🥐`; // update the counter display
+  function updateDisplay() {
+    if (counterElement) {
+      counterElement.textContent = `You have ${counter} crossiants!🥐`; // update the counter display
+    }
+    if (counterElement) {
+      const visible = Math.floor(counter);
+      counterElement.textContent = `You have ${visible} crossiants!🥐`; // update the counter display
+    }
+    if (rateElement) {
+      rateElement.textContent =
+        `You are making ${cookscrossiants} crossiants per second!🥐`; // update the rate display
+    }
+    if (bakerButton) {
+      bakerButton.textContent = `10 crossiants = 1 Baker👩‍🍳 (Owned: ${bakers})`; // update the baker button display
+    }
   }
-  if (counterElement) {
-    const visible = Math.floor(counter);
-    counterElement.textContent = `You have ${visible} crossiants!🥐`; // update the counter display
-  }
-  if (rateElement) {
-    rateElement.textContent =
-      `You are making ${cookscrossiants} crossiants per second!🥐`; // update the rate display
-  }
-}
-// function updateCookDisplay() {
-//   if (cookElement) {
-//     cookElement.textContent =
-//       `Click to hire a baker!👩‍🍳 You have ${cookscrossiants} bakers!👩‍🍳`; // update the cook display
-//   }
-// };
